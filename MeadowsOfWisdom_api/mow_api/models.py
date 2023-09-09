7bb921001266d51b2dea90c9bfa3ba6353792d7d
MichaelAngeloTheFirst <siemic13@gmail.com>
2023-09-09 19:24:56 +0200

    funfact serializer logic

---
+++ MeadowsOfWisdom_api/mow_api/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class TimeTrackedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Reaction(models.Model):
+    upvote = models.PositiveIntegerField(default=0)
+    downvote = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        abstract = True
+
+
+class FunFact(TimeTrackedModel, Reaction):
+    author = models.ForeignKey(
+        User, related_name='facts', on_delete=models.CASCADE)
+    fact_text = models.TextField()
+
+    class Meta:
+        verbose_name = "Fun Fact"
+        verbose_name_plural = "Fun Facts"
+
+    def __repr__(self) -> str:
+        return f"<FunFact author= {self.author.username} fact_text= {self.fact_text} date= {self.date} >"
+
+    def __str__(self):
+        return self.fact_text
+
+
+class FunFactComment(TimeTrackedModel, Reaction):
+    author = models.ForeignKey(
+        User, related_name='comments', on_delete=models.CASCADE)
+    fact = models.ForeignKey(
+        FunFact, related_name='comments', on_delete=models.CASCADE)
+    comment_text = models.TextField()
+
+    def __str__(self) -> str:
+        return self.comment_text
--- MeadowsOfWisdom_api/mow_api/serializers.py
+++ MeadowsOfWisdom_api/mow_api/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from mow_api.models import FunFact
+
+
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -19,3 +22,13 @@ class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ["id", "name"]
+
+
+class FunFactSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FunFact
+        fields = ["id", "fact_text", "upvote",
+                  "downvote", "created_at", "updated_at"]
+        read_only_fields = ["id", "upvote",
+                            "downvote",  "created_at", "updated_at"]
